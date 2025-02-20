431102c0ca324510d87dc71b4edb60f0e8bab143
Kareem Farid <kareefardi@users.noreply.github.com>
2025-02-20 15:15:39 +0200

    Add workflows to run BusWrap on IPs (#19)
    * test ci
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * debug
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * fix matrix definition
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * fix wrong ip repo reference
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * pass the repo to checkout
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * modify buswrap to cause a diff
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * test
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * clone with token
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * test
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * update GITHUB_TOKEN
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * remove token
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * use OL_BOT_GITHUB_TOKEN
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * change username to local
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * try EFSTAFF_TOKEN
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * updates
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * fixes
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * skip for branches made by ci
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * renames and cleanup
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * add more ips + some renames
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * update regression
    add GITHUB_STEP_SUMMARY
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * disable i2c false
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    * on push to main
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Kareem Farid <kareefardi@users.noreply.github.com>

---
+++ .github/scripts/get_ips_matrix.py
@@ -0,0 +1,31 @@
+import yaml
+import json
+
+
+def main():
+    test_set_yaml = ".github/ips.yaml"
+    output_matrix = {"ips": []}
+    test_set_stream = open(test_set_yaml)
+    data = yaml.load(test_set_stream, Loader=yaml.Loader)
+    for item in data:
+        name = item["name"]
+        repo = item["repo"]
+        fw = item.get("fw", "true")
+        docs = item.get("docs", "true")
+        bus_wrapper = item.get("bus_wrapper", "true")
+        output_matrix["ips"].append(
+            {
+                "name": name,
+                "repo": repo,
+                "fw": str(fw).lower(),
+                "docs": str(docs).lower(),
+                "bus_wrapper": str(bus_wrapper).lower(),
+            }
+        )
+
+    print(json.dumps(output_matrix))
+    test_set_stream.close()
+
+
+if __name__ == "__main__":
+    main()
